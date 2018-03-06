e5b4d7c78aeef223e89c9ffc478f76a28d351248
JoeyTribbiani <vophanhongdung@gmail.com>
2018-03-06 08:52:49 +0700

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Dũng Võ on 3/6/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
